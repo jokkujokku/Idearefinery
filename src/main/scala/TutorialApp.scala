f606220045f2b91bc88d7b5690f9c44afd1ffb08
Imran Mamin <imran.mamin@aalto.fi>
2022-11-05 22:21:30 +0200

    tiedoston index.html sijainti muutettu

--- src/main/scala/TutorialApp.scala
+++ src/main/scala/TutorialApp.scala
@@ -7,20 +7,5 @@ import java.io.File
 
 val filename = "src/main/HTML/index.html"
 
-object TutorialApp:
-  def test = ???
 
-  def appendPar(targetNode: dom.Node, text: String): Unit =
-    val parNode = document.createElement("p")
-    parNode.textContent = text
-    targetNode.appendChild(parNode)
-
-
-  def main(args: Array[String]): Unit =
-    appendPar(document.body, "Hello world!")
-
-  def getData(currentFile: File) =
-    val name = currentFile.getElementById("name")
-    val password = currentDocument.getElementById("password")
-    val usersFile = "src/main/scala/Backend/users"
 
